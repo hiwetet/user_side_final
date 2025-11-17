4c48e7d50b9d9bb43d5ae062299e6962f0ec5093
Hiwete <hiwetet@gmail.com>
2025-11-17 18:15:41 -0500

    Copy from old repo and download initializr

---
+++ CafeQuest/src/test/java/com/example/CafeQuest/CafeQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
